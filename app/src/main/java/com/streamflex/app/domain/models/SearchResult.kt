38d1081b43c5fdd1a0147f0519af8b4e77b2a2be
flex-drivee <ia7465045@gmail.com>
2026-01-29 23:27:23 +0500

    updates

--- app/src/main/java/com/streamflex/app/domain/models/SearchResult.kt
+++ app/src/main/java/com/streamflex/app/domain/models/SearchResult.kt
@@ -1,7 +1,5 @@
 package com.streamflex.app.domain.models
 
-import android.R
-import com.streamflex.app.data.providers.hdhub4u.Hdhub4uProvider
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.InternalSerializationApi // 1. Add Import
 
@@ -13,6 +11,5 @@ data class SearchResult(
     val title: String,
     val poster: String? = null,
     val type: ContentType,
-    val year: Int? = null,
-    val provider: String
+    val year: Int? = null
 )
