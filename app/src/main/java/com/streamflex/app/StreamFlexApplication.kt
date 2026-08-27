e69840db26956bd89d9f78cfbbf3346705bbef9a
flex-drivee <ia7465045@gmail.com>
2026-08-27 18:12:40 +0000

    feat: Add visible WebView CAPTCHA dialog
    Upgraded WebViewResolver to display a visual AlertDialog for CAPTCHAs that require manual user interaction (like Cloudflare Turnstile checkboxes). Safely tracks top Activity via lifecycle callbacks to ensure dialogs can be launched globally.

--- app/src/main/java/com/streamflex/app/StreamFlexApplication.kt
+++ app/src/main/java/com/streamflex/app/StreamFlexApplication.kt
@@ -14,11 +14,28 @@ class StreamFlexApplication : Application() {
     companion object {
         lateinit var instance: StreamFlexApplication
             private set
+            
+        var topActivity: android.app.Activity? = null
+            private set
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        registerActivityLifecycleCallbacks(object : android.app.Application.ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: android.app.Activity, savedInstanceState: android.os.Bundle?) {}
+            override fun onActivityStarted(activity: android.app.Activity) {}
+            override fun onActivityResumed(activity: android.app.Activity) {
+                topActivity = activity
+            }
+            override fun onActivityPaused(activity: android.app.Activity) {
+                if (topActivity == activity) topActivity = null
+            }
+            override fun onActivityStopped(activity: android.app.Activity) {}
+            override fun onActivitySaveInstanceState(activity: android.app.Activity, outState: android.os.Bundle) {}
+            override fun onActivityDestroyed(activity: android.app.Activity) {}
+        })
 
         initializeModules()
     }
